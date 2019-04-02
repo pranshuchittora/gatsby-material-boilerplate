0bbf3c27486605d918cc4d56cb320e750c50e33e
Pranshu Chittora <pranshuchittora17@gmail.com>
2019-04-03 00:55:40 +0530

    feature: removes unwanted code
    removes unwanted gatsby code, adds material ui & other packages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
+import { CircularProgress } from "@material-ui/core"
+import TrumpImage from "../images/dtrump.png"
+import "../style/index.css"
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <React.Fragment>
+    <div class="rotate-div">
+      <img class="rotate-infinite" src={TrumpImage} />
+      <br />
+      <br />
+      <CircularProgress color="primary" />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </React.Fragment>
 )
 
 export default IndexPage
