13e2886d3f140b202081f12d3fee0ea20301bd13
Pranshu Chittora <pranshuchittora17@gmail.com>
2019-05-08 04:46:02 +0530

    chore(GA): adds GA id
    Created and added new GA tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-115756395-7",
+        trackingId: "UA-115756395-8",
       },
     },
     `gatsby-plugin-react-helmet`,
